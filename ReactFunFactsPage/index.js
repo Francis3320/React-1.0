a8f7cb0ff9fc057015abc3b5ffe5bff7dd1b4b0c
Francis3320 <darkofrancis18@yahoo.com>
2023-02-02 17:51:48 +0000

    Its composable

--- ReactFunFactsPage/index.js
+++ ReactFunFactsPage/index.js
@@ -1,1 +1,12 @@
-ReactDOM.render(<h1>Hello Boss</h1>, document.getElementById("root"))
+
+function MainContent(){
+    return (<h1>I am learning React</h1>)
+}
+
+ReactDOM.render(
+    <div>
+    
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+)
