abd5d9e712503f27852670fa56cc4de605203653
Francis3320 <darkofrancis18@yahoo.com>
2023-03-03 13:50:31 +0000

    NavBar

---
+++ airbnb-clone/src/components/NavBar.js
@@ -0,0 +1,12 @@
+
+import React from "react"
+
+function NavBar(){
+ return(
+     <div className="NavBar">
+        <img src={require("../images/airbnb.png")}  className="logo" alt="img not found" />
+     </div>
+ )   
+}
+
+export default NavBar;
